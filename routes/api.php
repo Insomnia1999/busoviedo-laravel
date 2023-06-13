e60a2e2b8cf80c0dc47d4aef907078d30640fe13
Insomnia1999 <ivanin577@gmail.com>
2023-06-13 16:53:19 +0200

    Add import to routescontroller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Stop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoutesController;
 
 /*
 |--------------------------------------------------------------------------
