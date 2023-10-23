37ea0ef0ecc9f7786f69fdc160d5a2882cad92d1
Insomnia1999 <ivanin577@gmail.com>
2023-10-23 14:46:30 +0200

    Change line table name

--- app/Models/Line.php
+++ app/Models/Line.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Line extends Model
 {
-    protected $table = "lines";
+    protected $table = "bus_lines";
     protected $primaryKey = "id";
     public $timestamps = false;
 }
